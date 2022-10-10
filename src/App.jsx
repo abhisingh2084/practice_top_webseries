e7bb9b9e1acec38d0690c0348d0503f28e4d2a58
Abhishek kumar <abhisingh2084@gamil.com>
2022-10-10 12:15:39 +0530

    practice

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Cards from './Cards'
+
+function App(){
+    return(
+    <>
+    <Cards 
+    imgsrc = "https://m.behindwoods.com/tamil-movies/slideshow/best-web-series-to-watch-on-amazon-prime-video-hotstar-netflix/images/series-dark.jpg"
+    title = "A Netflix orginal Series"
+    sname = 'Dark'
+    link = ""
+    />
+    <Cards/>
+    <Cards/>
+    <Cards/>
+    <Cards/>
+
+    </>);
+}
+
+export default App;
